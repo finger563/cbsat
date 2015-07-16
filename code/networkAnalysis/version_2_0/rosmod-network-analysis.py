f773c98ef26adad87228caa3ce4fcacc9240b48a
William Emfinger <waemfinger@gmail.com>
2015-07-16 15:57:59 -0500

    updated rosmod library code.

--- code/networkAnalysis/version_2_0/rosmod-network-analysis.py
+++ code/networkAnalysis/version_2_0/rosmod-network-analysis.py
@@ -6,23 +6,29 @@ def AnalyzeHost(host, nodes, period, numPeriods, nc_mode = False, nc_step = 1):
         period,
         numPeriods)
 
-    provided = Profile('provided',period,
-                       host.properties['system_network_profile'],
-                       numPeriods)
+    provided = Profile(
+        kind = 'provided',
+        period = period )
+    provided.BuildProfile(
+        prof_str = host.properties['system_network_profile'],
+        num_periods = numPeriods )
     required = Profile('required',period)
     for node in nodes:
         for compInst in node.children:
             for port in compInst.properties['component_reference'].children:
                 if 'port_network_profile' in port.properties.keys():
-                    tmpProfile = Profile('required',period,
-                                         port.properties['port_network_profile'],
-                                         numPeriods)
+                    tmpProfile = Profile(
+                        kind = 'required',
+                        period = period )
+                    tmpProfile.BuildProfile(
+                        prof_str = port.properties['port_network_profile'],
+                        num_periods = numPeriods)
                     required.AddProfile(tmpProfile)
     required.Integrate()
     provided.Integrate()
     if nc_mode:
-        required.ConvertToNC(nc_step, lambda x,y: max(x,y))
-        provided.ConvertToNC(nc_step, lambda x,y: min(x,y))
+        required.ConvertToNC(nc_step, lambda l: max(l))
+        provided.ConvertToNC(nc_step, lambda l: min(l))
     output, maxBuffer, maxDelay = required.Convolve(provided)
     print "\n[Time location, buffersize]:",[maxBuffer[0], maxBuffer[2]]
     print "[Time location, delay]:",[maxDelay[0], maxDelay[2]]
