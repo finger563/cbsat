effc74e1b7e281e8389d04ebcb29fc9b99f0bee2
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-07-17 00:05:49 -0500

    Working on fixing documentation.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -1,7 +1,7 @@
-project = 'kRPC'
-version = '0.1'
-release = '0.1.10'
-copyright = '2015, djungelorm'
+project = 'CBSAT'
+version = '0.2'
+release = '0.2.0'
+copyright = '2015, finger563'
 
 master_doc = 'index'
 source_suffix = '.rst'
@@ -13,9 +13,9 @@ pygments_style = 'sphinx'
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-htmlhelp_basename = 'krpc-doc'
+htmlhelp_basename = 'cbsat-doc'
 html_static_path = ['crafts','scripts','_static']
-html_context = { 'css_files': ['_static/custom.css'] }
+html_context = { 'css_files': ['./_static/custom.css'] }
 
 todo_include_todos = True
 
