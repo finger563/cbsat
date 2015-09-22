b488cb8b0328880ac1d10dcfda6e1d4c5bd9af5e
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-09-21 20:22:35 -0500

    added priority to htb impl.

--- src/analysis/v2.0/routing_test/tc_config_router.sh
+++ src/analysis/v2.0/routing_test/tc_config_router.sh
@@ -30,8 +30,8 @@ $TC qdisc add dev ${DEV} parent 1:2 handle 12: pfifo
 $TC qdisc add dev ${DEV} parent 11:1  handle 2: htb
 
 $TC class add dev ${DEV} parent 2: classid 2:1 htb rate 10Mbit ceil 10Mbit
-$TC class add dev ${DEV} parent 2:1 classid 2:10 htb rate 10Mbit ceil 10Mbit
-$TC class add dev ${DEV} parent 2:1 classid 2:20 htb rate 10bit ceil 10Mbit
+$TC class add dev ${DEV} parent 2:1 classid 2:10 htb rate 10Mbit ceil 10Mbit prio 0
+$TC class add dev ${DEV} parent 2:1 classid 2:20 htb rate 10bit ceil 10Mbit prio 1
 
 $TC qdisc add dev ${DEV} parent 2:10 handle 21: pfifo
 $TC qdisc add dev ${DEV} parent 2:20 handle 22: pfifo
