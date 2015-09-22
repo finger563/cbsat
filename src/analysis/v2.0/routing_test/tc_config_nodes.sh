8d007644c220273f94ac63f4d4b7e5143ed66e4a
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-09-22 18:17:35 -0500

    updated node tc to use htb for better performance.

--- src/analysis/v2.0/routing_test/tc_config_nodes.sh
+++ src/analysis/v2.0/routing_test/tc_config_nodes.sh
@@ -27,8 +27,8 @@ $TC qdisc add dev ${DEV} root handle 1: prio
 $TC qdisc add dev ${DEV} parent 1:1 handle 11: netem delay 100ms
 $TC qdisc add dev ${DEV} parent 1:2 handle 12: pfifo
 
-$TC qdisc add dev ${DEV} parent 11:1 handle 111: tbf \
-    rate 1Mbit peakrate 1001kbit mtu 8192 latency 100s burst 154000000
+$TC qdisc add dev ${DEV} parent 11:1 handle 111: htb
+$TC class add dev ${DEV} parent 111: classid 111:1 htb rate 1Mbit ceil 1Mbit
 
 # FILTER APPLICATION TRAFFIC VERSUS NON APP TRAFIC
 $TC filter add dev ${DEV} protocol ip parent 1: prio 1 u32 \
@@ -37,11 +37,11 @@ $TC filter add dev ${DEV} protocol ip parent 1: prio 1 u32 \
 $TC filter add dev ${DEV} protocol ip parent 1: prio 1 u32 \
     match ip dst 10.1.1.0/24 flowid 1:1
 
-$TC filter add dev ${DEV} protocol ip parent 1: prio 1 u32 \
-    match ip dst 192.168.122.2 flowid 1:1
+$TC filter add dev ${DEV} protocol ip parent 111: prio 1 u32 \
+    match ip src 10.1.1.0/24 flowid 111:1
 
-$TC filter add dev ${DEV} protocol ip parent 1: prio 1 u32 \
-    match ip dst 192.168.122.4 flowid 1:1
+$TC filter add dev ${DEV} protocol ip parent 111: prio 1 u32 \
+    match ip dst 10.1.1.0/24 flowid 111:1
 
 $TC filter add dev ${DEV} protocol ip parent 1: prio 2 u32 \
     match ip src 192.168.122.0/24 flowid 1:2
