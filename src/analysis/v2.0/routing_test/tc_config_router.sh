75ac8ba0d591bd6ede34c74f07370cfb4b72515e
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-09-21 20:14:21 -0500

    updated router tc code to use htb instead of tbf; seems like ti might work like we need.

--- src/analysis/v2.0/routing_test/tc_config_router.sh
+++ src/analysis/v2.0/routing_test/tc_config_router.sh
@@ -27,12 +27,14 @@ $TC qdisc add dev ${DEV} root handle 1: prio
 $TC qdisc add dev ${DEV} parent 1:1 handle 11: netem delay 100ms
 $TC qdisc add dev ${DEV} parent 1:2 handle 12: pfifo
 
-$TC qdisc add dev ${DEV} parent 11:1 handle 111: tbf \
-    rate 1Mbit peakrate 1001kbit mtu 8192 latency 100s burst 154000000
-$TC qdisc add dev ${DEV} parent 111:1  handle 2: prio
+$TC qdisc add dev ${DEV} parent 11:1  handle 2: htb
 
-$TC qdisc add dev ${DEV} parent 2:1 handle 21: pfifo
-$TC qdisc add dev ${DEV} parent 2:2 handle 22: pfifo
+$TC class add dev ${DEV} parent 2: classid 2:1 htb rate 10Mbit ceil 10Mbit
+$TC class add dev ${DEV} parent 2:1 classid 2:10 htb rate 10Mbit ceil 10Mbit
+$TC class add dev ${DEV} parent 2:1 classid 2:20 htb rate 10bit ceil 10Mbit
+
+$TC qdisc add dev ${DEV} parent 2:10 handle 21: pfifo
+$TC qdisc add dev ${DEV} parent 2:20 handle 22: pfifo
 
 # FILTER APPLICATION TRAFFIC VERSUS NON APP TRAFIC
 $TC filter add dev ${DEV} protocol ip parent 1: prio 1 u32 \
@@ -48,9 +50,9 @@ $TC filter add dev ${DEV} protocol ip parent 1: prio 2 u32 \
     match ip src 192.168.122.0/24 flowid 1:2
 
 # PRIORITIZE CERTAIN APPLICATION TRAFFIC
-$TC filter add dev ${DEV} protocol ip parent 2: prio 1 u32 \
-    match ip src 10.1.1.1 flowid 2:1
+$TC filter add dev ${DEV} protocol ip parent 2:0 prio 1 u32 \
+    match ip src 10.1.1.1 flowid 2:10
 
-$TC filter add dev ${DEV} protocol ip parent 2: prio 2 u32 \
-    match ip src 10.1.1.3 flowid 2:2
+$TC filter add dev ${DEV} protocol ip parent 2:0 prio 2 u32 \
+    match ip src 10.1.1.3 flowid 2:20
 
