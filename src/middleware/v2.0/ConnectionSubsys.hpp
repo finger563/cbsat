bf9984af24590e370c742bdb35f5159d271b8221
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-09-22 23:31:28 -0500

    added ipv4 connection

--- src/middleware/v2.0/ConnectionSubsys.hpp
+++ src/middleware/v2.0/ConnectionSubsys.hpp
@@ -105,6 +105,179 @@ public:
   }
 };
 
+class IPV4Connection : public Connection {
+public:
+  int sockfd;
+  struct sockaddr_in local_addr;
+  struct sockaddr_in remote_addr;
+
+  IPV4Connection()
+    : Connection(true)
+  {
+    serverPort = 7777;
+    receiveTimeout = 5;
+    serverIP = "10.1.1.1";
+  }
+
+  IPV4Connection(const IPV4Connection &s)
+    : Connection(s),
+      sockfd(s.sockfd),
+      local_addr(s.local_addr),
+      remote_addr(s.remote_addr)
+  {
+  }
+
+  IPV4Connection & operator= (const IPV4Connection &s)
+  {
+    if (&s != this)
+      {
+        IPV4Connection tmp (s);
+        swap (tmp);
+      }
+    return *this;
+  }
+
+  virtual IPV4Connection* clone() const 
+  {
+    return new IPV4Connection( *this );
+  }
+
+  virtual void swap (IPV4Connection &s)
+  {
+    std::swap (sockfd, s.sockfd);
+    std::swap (local_addr, s.local_addr);
+    std::swap (remote_addr, s.remote_addr);
+  }
+
+  virtual void Close()
+  {
+    close(sockfd);
+  }
+
+  ~IPV4Connection() 
+  {
+    Close();
+  }
+
+  virtual long send(const char *buffer, long len) {
+    long bytes;
+    if ((bytes = sendto(sockfd, buffer, len, 0, (struct sockaddr *) &remote_addr, sizeof(remote_addr))) == -1 ) {
+      int errsv = errno;
+      TG_LOG("ERROR : Couldn't send : %s\n", strerror(errsv) );
+    }
+    int size;
+    int error = ioctl(sockfd, SIOCOUTQ, &size);
+    if ( size > bufferSize ) 
+      bufferSize = size;
+    return bytes;
+  }
+
+  virtual long receive(char *buffer, long len) {
+    socklen_t remote_addr_len = sizeof(remote_addr);
+    long bytes;
+    if ((bytes = recvfrom(sockfd, buffer, len,0,(struct sockaddr *)&remote_addr, &remote_addr_len)) == -1) {
+      int errsv = errno;
+      TG_LOG("ERROR: Haven't received response! : %s!\n", strerror(errsv));
+    }
+    return bytes;
+  }
+
+  virtual int InitializeServer() {
+    TG_LOG("Initializing IPV4 Server\n");
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) <0) {
+      int errsv = errno;
+      TG_LOG("ERROR opening socket : %s\n", strerror(errsv));
+      return -1;
+    }
+
+    int optval = 1;
+    int retval = 0;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(optval))!=0) {
+      TG_LOG("ERROR setting sockopt : %d, %s\n",retval, strerror(retval));
+      return -1;
+    }
+    
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port = htons(serverPort);
+    
+    if ( (retval = inet_pton(AF_INET, serverIP.c_str(), (void *)&(local_addr.sin_addr.s_addr))) !=1) {
+      TG_LOG("ERROR: inet_pton : %d, %s\n",retval, strerror(retval));
+      return -1;
+    }  
+    
+    if (bind(sockfd,(struct sockaddr *)&local_addr, sizeof(local_addr))<0) {
+      TG_LOG("Couldn't bind!");
+      close(sockfd);
+      return -1;
+    }
+    
+    struct timeval tv;
+    tv.tv_sec = receiveTimeout;
+    tv.tv_usec = 0;
+    if ( (retval=setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv))) < 0) {
+      TG_LOG("Couldn't set sockopts : %d, %s\n",retval, strerror(retval));
+      close(sockfd);
+      return -1;
+    }
+
+    TG_LOG ( "Waiting on %s:%d\n",serverIP.c_str(),serverPort);
+    return 0;
+  }
+  
+  virtual int InitializeClient() {
+    TG_LOG("Initializing IPV4 Client\n");
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) <0) {
+      int errsv = errno;
+      TG_LOG("ERROR opening socket : %s\n", strerror(errsv));
+      return -1;
+    }
+  
+    int optval = 1;
+    int retval = 0;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(optval))!=0) {
+      int errsv = errno;
+      TG_LOG("ERROR setting sockopt : %s\n", strerror(errsv));
+      return -1;
+    }
+  
+    remote_addr.sin_family = AF_INET;
+    remote_addr.sin_port = htons(serverPort);
+    
+    if ( (retval = inet_pton(AF_INET, serverIP.c_str(), (void *)&(remote_addr.sin_addr.s_addr))) !=1) {
+      TG_LOG("ERROR: inet_pton : %d\n",retval);
+      return -1;
+    }
+  
+    struct timeval tv;
+    tv.tv_sec = receiveTimeout;
+    tv.tv_usec = 0;
+    if ( (retval=setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv))) < 0) {
+      TG_LOG("Couldn't set sockopts : %d, %s\n",retval, strerror(retval));
+      close(sockfd);
+      return -1;
+    }
+    TG_LOG ( "Connecting to %s:%d\n",serverIP.c_str(),serverPort);
+    return 0;
+  }  
+
+  void *get_in_addr(struct sockaddr *sa){
+    if (sa->sa_family == AF_INET){
+      return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+  }
+
+  u_short get_in_port(struct sockaddr *sa)
+  {
+    if (sa->sa_family == AF_INET) {
+      return ((struct sockaddr_in*)sa)->sin_port;
+    }
+
+    return ((struct sockaddr_in6*)sa)->sin6_port;
+  }
+
+};
+
 class IPV6_Connection : public Connection {
 public:
   int sockfd;
