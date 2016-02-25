2dc99aa5ce4086a11590783b5379acfb059efb0f
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-02-25 11:56:29 -0600

    minor updates.

--- src/analysis/v2.0/routing_test/tc_config_nodes.sh
+++ src/analysis/v2.0/routing_test/tc_config_nodes.sh
@@ -1,12 +1,66 @@
 #!/bin/bash
 
 TC=/sbin/tc
-
 DEV=eth0
 
-USETBF="false"
+USETBF="true"
+BW=70000
+DELAY=0
+BUCKET=100
+BUFFER=10000
 
-if [[ "$1" = "status" ]]
+PRINT_STATUS="false"
+CLEAR="false"
+
+for i in "$@"
+do
+    case $i in
+	-p|--print)
+	    PRINT_STATUS="true"
+	    shift # past argument=value
+	    ;;
+	--clear)
+	    CLEAR="true"
+	    shift # past argument=value
+	    ;;
+	-d=*|--delay=*)
+	    DELAY="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	-b=*|--bandwidth=*)
+	    BW="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	-k=*|--bucket=*)
+	    BUCKET="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	-f=*|--buffer=*)
+	    BUFFER="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	--use_tbf)
+	    USETBF="true"
+	    shift # past argument with no value
+	    ;;
+	--use_htb)
+	    USETBF="false"
+	    shift # past argument with no value
+	    ;;
+	*)
+	    # unknown option
+	    ;;
+    esac
+    done
+
+echo "Using options:"
+echo "  use_tbf:   ${USETBF}"
+echo "  bandwidth: ${BW}"
+echo "  delay:     ${DELAY}"
+echo "  bucket:    ${BUCKET}"
+echo "  buffer:    ${BUFFER}"
+
+if [[ "$PRINT_STATUS" = "true" ]]
 then
     $TC -s qdisc ls dev $DEV
     $TC -s class ls dev $DEV
@@ -17,53 +71,51 @@ fi
 $TC qdisc del dev $DEV root    2> /dev/null > /dev/null
 $TC qdisc del dev $DEV ingress 2> /dev/null > /dev/null
 
-if [[ "$1" = "stop" ]]
+if [[ "$CLEAR" = "true" ]]
 then
     exit
 fi
 
+let "PEAKBW=${BW}+10"
+let "BW2=${BW}+${BW}"
+
 ###### uplink
 
 $TC qdisc add dev ${DEV} root handle 1: prio
-
-$TC qdisc add dev ${DEV} parent 1:1 handle 11: netem delay 100ms
+$TC qdisc add dev ${DEV} parent 1:1 handle 11: netem delay ${DELAY}ms
 $TC qdisc add dev ${DEV} parent 1:2 handle 12: pfifo
 
 if [[ "$USETBF" = "true" ]]
 then
-    $TC qdisc add dev ${DEV} parent 11:1 handle 111: tbf \
-	rate 1Mbit burst 10kb latency 100000ms peakrate 1.01mbit mtu 1540
+    $TC qdisc add dev ${DEV} parent 11:1 handle 2: tbf \
+	rate ${BW}bit limit ${BUFFER}k burst ${BUCKET}
 else
-    $TC qdisc add dev ${DEV} parent 11:1 handle 2: htb
-
-    $TC class add dev ${DEV} parent 2: classid 2:1 htb rate 10Mbit ceil 10Mbit
-    $TC class add dev ${DEV} parent 2:1 classid 2:10 htb rate 10Mbit ceil 10Mbit
-
+    $TC qdisc add dev ${DEV} parent 11:1 handle 2: htb 
+    $TC class add dev ${DEV} parent 2: classid 2:1 htb rate ${BW2}bit
+    $TC class add dev ${DEV} parent 2:1 classid 2:10 htb rate ${BW}bit ceil ${PEAKBW}bit
     $TC qdisc add dev ${DEV} parent 2:10 handle 21: pfifo
+    # ceil ${PEAKBW}bit # burst ${BUCKET # cburst 10
+    # burst ${BUCKET} # cburst 10
 fi
+
 echo "set qdiscs up"
 
 # FILTER APPLICATION TRAFFIC VERSUS NON APP TRAFIC
 $TC filter add dev ${DEV} protocol ip parent 1: prio 1 u32 \
-    match ip dst 10.1.1.2 flowid 1:1
-$TC filter add dev ${DEV} protocol ip parent 1: prio 1 u32 \
-    match ip dst 10.1.1.4 flowid 1:1
+    match ip dst 10.1.1.0/24 flowid 1:1
 
 if [[ "$USETBF" = "true" ]]
 then
     echo ""
 else
     $TC filter add dev ${DEV} protocol ip parent 2: prio 1 u32 \
-	match ip dst 10.1.1.2 flowid 2:10
-
-    $TC filter add dev ${DEV} protocol ip parent 2: prio 1 u32 \
-	match ip dst 10.1.1.4 flowid 2:10
+	match ip dst 10.1.1.0/24 flowid 2:10
 fi
 
 echo "set priority filters up"
 
 $TC filter add dev ${DEV} protocol ip parent 1: prio 2 u32 \
     match ip src 192.168.122.0/24 flowid 1:2
-$TC filter add dev ${DEV} protocol ip parent 1: prio 2 u32 \
-    match ip src 10.1.1.0/24 flowid 1:2
+#$TC filter add dev ${DEV} protocol ip parent 1: prio 2 u32 \
+#    match ip src 10.1.1.0/24 flowid 1:2
 echo "set other filters up"
--- src/analysis/v2.0/routing_test/tc_config_router.sh
+++ src/analysis/v2.0/routing_test/tc_config_router.sh
@@ -1,12 +1,66 @@
 #!/bin/bash
 
 TC=/sbin/tc
-
 DEV=eth0
 
 USETBF="false"
+BW=70000
+DELAY=0
+BUCKET=100
+BUFFER=10000
 
-if [[ "$1" = "status" ]]
+PRINT_STATUS="false"
+CLEAR="false"
+
+for i in "$@"
+do
+    case $i in
+	-p|--print)
+	    PRINT_STATUS="true"
+	    shift # past argument=value
+	    ;;
+	--clear)
+	    CLEAR="true"
+	    shift # past argument=value
+	    ;;
+	-d=*|--delay=*)
+	    DELAY="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	-b=*|--bandwidth=*)
+	    BW="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	-k=*|--bucket=*)
+	    BUCKET="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	-f=*|--buffer=*)
+	    BUFFER="${i#*=}"
+	    shift # past argument=value
+	    ;;
+	--use_tbf)
+	    USETBF="true"
+	    shift # past argument with no value
+	    ;;
+	--use_htb)
+	    USETBF="false"
+	    shift # past argument with no value
+	    ;;
+	*)
+	    # unknown option
+	    ;;
+    esac
+    done
+
+echo "Using options:"
+echo "  use_tbf:   ${USETBF}"
+echo "  bandwidth: ${BW}"
+echo "  delay:     ${DELAY}"
+echo "  bucket:    ${BUCKET}"
+echo "  buffer:    ${BUFFER}"
+
+if [[ "$PRINT_STATUS" = "true" ]]
 then
     $TC -s qdisc ls dev $DEV
     $TC -s class ls dev $DEV
@@ -17,51 +71,42 @@ fi
 $TC qdisc del dev $DEV root    2> /dev/null > /dev/null
 $TC qdisc del dev $DEV ingress 2> /dev/null > /dev/null
 
-if [[ "$1" = "stop" ]]
+if [[ "$CLEAR" = "true" ]]
 then
     exit
 fi
 
+let "PEAKBW=${BW}+10"
+let "BW2=${BW}+${BW}"
+
 ###### uplink
 
 $TC qdisc add dev ${DEV} root handle 1: prio
-
-$TC qdisc add dev ${DEV} parent 1:1 handle 11: netem delay 100ms
+$TC qdisc add dev ${DEV} parent 1:1 handle 11: netem delay ${DELAY}ms
 $TC qdisc add dev ${DEV} parent 1:2 handle 12: pfifo
 
 if [[ "$USETBF" = "true" ]]
 then
     $TC qdisc add dev ${DEV} parent 11:1 handle 2: tbf \
-	rate 1Mbit burst 10kb latency 100000ms peakrate 1.01mbit mtu 1540
+	rate ${BW}bit limit ${BUFFER}k burst ${BUCKET}
 
     $TC qdisc add dev ${DEV} parent 2: handle 21: prio
-
     $TC qdisc add dev ${DEV} parent 21:1 handle 211: pfifo
     $TC qdisc add dev ${DEV} parent 21:2 handle 212: pfifo
 else
     $TC qdisc add dev ${DEV} parent 11:1  handle 2: htb
-    
-    $TC class add dev ${DEV} parent 2: classid 2:1 htb rate 10Mbit ceil 10Mbit
-    $TC class add dev ${DEV} parent 2:1 classid 2:10 htb rate 10Mbit ceil 10Mbit prio 0
-    $TC class add dev ${DEV} parent 2:1 classid 2:20 htb rate 10bit ceil 10Mbit prio 1
+    $TC class add dev ${DEV} parent 2: classid 2:1 htb rate ${BW2}bit
+    $TC class add dev ${DEV} parent 2:1 classid 2:10 htb rate ${BW}bit ceil ${PEAKBW}bit prio 0
+    $TC class add dev ${DEV} parent 2:1 classid 2:20 htb rate ${BW}bit ceil ${PEAKBW}bit prio 1
     
     $TC qdisc add dev ${DEV} parent 2:10 handle 21: pfifo
     $TC qdisc add dev ${DEV} parent 2:20 handle 22: pfifo
 fi
 echo "set qdiscs up"
 
-# FILTER APPLICATION TRAFFIC VERSUS NON APP TRAFIC
+# FILTER APPLICATION TRAFFIC
 $TC filter add dev ${DEV} protocol ip parent 1: prio 1 u32 \
-    match ip src 10.1.1.1 flowid 1:1
-$TC filter add dev ${DEV} protocol ip parent 1: prio 1 u32 \
-    match ip src 10.1.1.3 flowid 1:1
-echo "set priority filters up"
-
-$TC filter add dev ${DEV} protocol ip parent 1: prio 2 u32 \
-    match ip src 10.1.1.0/24 flowid 1:2
-$TC filter add dev ${DEV} protocol ip parent 1: prio 2 u32 \
-    match ip src 192.168.122.0/24 flowid 1:2
-echo "set other filters up"
+    match ip src 10.1.1.0/24 flowid 1:1
 
 # PRIORITIZE CERTAIN APPLICATION TRAFFIC
 if [[ "$USETBF" = "true" ]]
@@ -78,4 +123,10 @@ else
     $TC filter add dev ${DEV} protocol ip parent 2: prio 2 u32 \
 	match ip src 10.1.1.3 flowid 2:20
 fi
-echo "set priority filters up"
+echo "set application filters up"
+
+# FILTER NON APPLICATION TRAFFIC
+$TC filter add dev ${DEV} protocol ip parent 1: prio 2 u32 \
+    match ip src 191.168.127.0/24 flowid 1:2
+echo "set non-application filters up"
+
